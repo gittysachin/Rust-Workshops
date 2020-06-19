08c0833bf3402251f25239f9934b6786a4bd9499
Sachin Jangid <34537025+gittysachin@users.noreply.github.com>
2020-06-19 20:17:49 +0530

    While and Loop

--- Rust-Lang-Course/src/control_flow.rs
+++ Rust-Lang-Course/src/control_flow.rs
@@ -38,6 +38,28 @@ fn if_statement() {
   )
 }
 
+fn while_and_loop() {
+  let mut x = 1;
+  while x < 1000 {
+    x *= 2;
+    if x == 64 {
+      continue;
+    }
+    println!("x = {}", x);
+  }
+
+  let mut y = 1;
+  loop {
+    // while true
+    y *= 2;
+    printnl!("y = {}", y);
+    if y == 1 << 10 {
+      break;
+    }
+  }
+}
+
 fn control_flow() {
   if_statement();
+  while_and_loop();
 }
