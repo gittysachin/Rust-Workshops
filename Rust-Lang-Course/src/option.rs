f4b3f35da78084dfa5f017e7ab77b40f9fef0562
Sachin Jangid <34537025+gittysachin@users.noreply.github.com>
2020-06-20 17:02:21 +0530

    Options

---
+++ Rust-Lang-Course/src/option.rs
@@ -0,0 +1,22 @@
+pub fn option() {
+  // Option<T>
+
+  let x = 3.0;
+  let y = 2.0;
+
+  // Some(z) None
+
+  let result: Option<f64> = if x / y != 0.0 { Some(x / y) } else { None };
+
+  println!("{:?}", result);
+
+  match result {
+    Some(z) => println!("{}/{} = {}", x, y, z),
+    None => println!("Cannot divide {} by {}", x, y),
+  }
+
+  //    if let / while let
+  if let Some(z) = result {
+    println!("z = {}", z);
+  }
+}
