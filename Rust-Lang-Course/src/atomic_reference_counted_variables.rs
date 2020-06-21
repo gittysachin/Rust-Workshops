bc2601c368883a69fbb728ac970852392b1eae65
Sachin Jangid <34537025+gittysachin@users.noreply.github.com>
2020-06-21 17:02:07 +0530

    Atomic Reference-Counted Variables (Arc)

---
+++ Rust-Lang-Course/src/atomic_reference_counted_variables.rs
@@ -0,0 +1,30 @@
+use std::rc::Rc;
+use std::sync::Arc;
+use std::thread;
+
+struct Person {
+  name: Arc<String>,
+}
+
+impl Person {
+  fn new(name: Arc<String>) -> Person {
+    Person { name: name }
+  }
+
+  fn greet(&self) {
+    println!("Hi, my name is {}", self.name);
+  }
+}
+
+fn arc_demo() {
+  let name = Arc::new("Sachin".to_string());
+  let person = Person::new(name.clone());
+
+  let t = thread::spawn(move || {
+    person.greet();
+  });
+
+  println!("Name = {}", name);
+
+  t.join().unwrap();
+}
